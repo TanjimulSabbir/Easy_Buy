55bf88b37ee2503d53af13d19bc43e5f349787f6
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-05-13 13:23:31 +0600

    working on category dropdown menu

--- src/components/Home/Products/Products.jsx
+++ src/components/Home/Products/Products.jsx
@@ -2,6 +2,7 @@
 import { useGetProductsQuery } from "../../../Redux/Features/Products/productApi";
 import Error from "../../../UI/Error";
 import Loading from "../../../UI/Loading";
+import { CategoriesDropDown } from "../../../utils/CategoriesDropDown";
 import Product from "./Product/Product";
 
 export default function Products() {
@@ -11,11 +12,12 @@ export default function Products() {
     if (!isLoading && isError || allProducts?.products?.length === 0) content = <Error message="Product(s) not found!" />;
 
     if (!isLoading && !isError && allProducts?.products?.length > 0) {
-        const allCategores=allProducts.products.map(product=>product)
+        // const allCategores = allProducts.products.map(product => ({ category: product.category.cat, subCategory: product.category.sub, immediate: product.category.imd }))
+        // console.log(allCategores, "from products page")
         content = allProducts.products.map(product => <Product key={product.id} product={product} />)
     }
-    
+
     return (
-        <div>Products: {content}</div>
+        <div>{CategoriesDropDown}</div>
     )
 }
